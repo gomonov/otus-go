9f9fe4bca20d372981cca18b3b93b496d9c19d80
Gomonov <y.gomonov@yandex.ru>
2025-10-13 11:49:09 +0300

    hw11

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,58 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var timeout time.Duration
+
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "connection timeout")
+	flag.Parse()
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	client := NewTelnetClient(net.JoinHostPort(host, port), timeout, os.Stdin, os.Stdout)
+	if err := client.Connect(); err != nil {
+		fmt.Fprintf(os.Stderr, "connection error: %v\n", err)
+		os.Exit(1)
+	}
+	defer client.Close()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+
+	done := make(chan struct{}, 2)
+
+	go func() {
+		defer close(done)
+		if err := client.Send(); err != nil {
+			fmt.Fprintf(os.Stderr, "send error: %v\n", err)
+		}
+	}()
+
+	go func() {
+		defer close(done)
+		if err := client.Receive(); err != nil {
+			fmt.Fprintf(os.Stderr, "receive error: %v\n", err)
+		}
+	}()
+
+	// Ждём либо сигнала прерывания, либо завершения одной из горутин
+	select {
+	case <-ctx.Done():
+		fmt.Fprintln(os.Stderr, "\ninterrupted")
+	case <-done:
+	}
+
+	// Ждём завершения обеих горутин
+	<-done
 }
